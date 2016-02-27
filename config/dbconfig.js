654c3d1150a59bca8757d7ebcc98fb7342ba7601
HAEBEOM <haebeompark@naver.com>
2016-02-27 20:32:15 +0900

    1

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,10 +1,10 @@
 var config = {
-    "host": process.env.FMS_DB_SERVER,
-    "port": process.env.FMS_DB_PORT,
-    "user": process.env.FMS_DB_USERNAME,
-    "password": process.env.FMS_DB_PASSWORD,
-    "database": process.env.FMS_DB,
-	"ssl": "Amazon RDS",
+    "host": process.env.GREEN_DB_SERVER,
+    "port": process.env.GREEN_DB_PORT,
+    "user": process.env.GREEN_DB_USERNAME,
+    "password": process.env.GREEN_DB_PASSWORD,
+    "database": process.env.GREEN_DB,
+    "ssl": "Amazon RDS",
     "debug": true
 }
 
